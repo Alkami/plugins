0f68ab30b6d0b07c4d99132f66bcbd8be64d96bc
Tony Hristov <tony.hristov@alkami.com>
2022-09-26 15:08:52 -0500

    chore:DEV-139004 publish to cloudsmith (tony)

--- packages/webview_flutter/webview_flutter_web/CHANGELOG.md
+++ packages/webview_flutter/webview_flutter_web/CHANGELOG.md
@@ -1,3 +1,11 @@
+# 3.4.0
+
+- chore:DEV-139004 publish to cloudsmith
+
+# 3.3.0
+
+- chore:DEV-139004 publish to cloudsmith
+
 # 3.1.0
 
 - chore:DEV-139004 publish to cloudsmith
--- shell_scripts/authorize_cloudsmith.sh
+++ shell_scripts/authorize_cloudsmith.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-echo $1 | dart pub token add https://dart.cloudsmith.io/alkami/fluttercore/
-echo $1 | dart pub token add https://dart.cloudsmith.io/alkami/flutterpackages/
+echo $1 | dart pub token add https://dart.cloudsmith.io/alkami/flutter/
