b7522534df1f3403993890cb1bf43e2085525804
Micaiah Skolnick <mskolnickdev@gmail.com>
2023-03-06 16:00:52 -0500

    chore:DEV-144619 switch mplat_pr_tools to mplat_dart (#12)

--- shell_scripts/alkamiValidatePRIsReadyForMerge.sh
+++ shell_scripts/alkamiValidatePRIsReadyForMerge.sh
@@ -5,43 +5,28 @@ echo &&
 echo webview_flutter_platform_interface: &&
 cd webview_flutter_platform_interface &&
 flutter pub get &&
-flutter pub run mplat_pr_tools validatePRIsReadyForMerge \
-    --publishDryRun \
-    --minCoverage 59.75 &&
+mplat_dart validate-pr &&
 
 echo &&
 echo webview_flutter_android: &&
 cd ../webview_flutter_android &&
 flutter pub get &&
-flutter pub run mplat_pr_tools validatePRIsReadyForMerge \
-    --publishDryRun \
-    --minCoverage 28 &&
+mplat_dart validate-pr &&
 
 echo &&
 echo webview_flutter_wkwebview: &&
 cd ../webview_flutter_wkwebview &&
 flutter pub get &&
-flutter pub run mplat_pr_tools validatePRIsReadyForMerge \
-    --publishDryRun \
-    --minCoverage 59.35 &&
+mplat_dart validate-pr &&
 
 echo &&
 echo webview_flutter_web: &&
 cd ../webview_flutter_web &&
 flutter pub get &&
-flutter pub run mplat_pr_tools validatePRIsReadyForMerge \
-    --publishDryRun \
-    --no-test \
-    --no-generateCoverage &&
+mplat_dart validate-pr &&
 
 echo &&
-echo webview_flutter_platform_interface: &&
+echo webview_flutter: &&
 cd ../webview_flutter &&
 flutter pub get &&
-flutter pub run mplat_pr_tools validatePRIsReadyForMerge \
-    --publishDryRun \
-    --minCoverage 85.64 \
-    --no-validateVersion
-    # Passing '--no-validateVersion' because the check in mplat_pr_tools finds cloudsmith versions with the other packages
-    # because the other names contain "webview_flutter"
-    # So we're not validating the version until that is fixed in mplat_pr_tools
+mplat_dart validate-pr
