1f707292f6a5bf65df73bb96acd3facfdfda5927
Micaiah Skolnick <micaiah.skolnick@alkami.com>
2023-03-06 12:42:33 -0500

    chore:DEV-144619 switch mplat_pr_tools to mplat_dart

--- shell_scripts/alkamiPublish.sh
+++ shell_scripts/alkamiPublish.sh
@@ -10,9 +10,8 @@ echo &&
 echo webview_flutter_platform_interface: &&
 cd webview_flutter_platform_interface &&
 flutter pub get &&
-flutter pub run mplat_pr_tools publish \
-    --no-createGithubRelease &&
 # Do not create github release until the very last step
+mplat_dart publish --no-github-release &&
 echo 'sleeping for '$SLEEP_TIME' seconds to wait for package to synchronize because other packages depend on platform interface...' && 
 sleep $SLEEP_TIME &&
 
@@ -20,25 +19,22 @@ echo &&
 echo webview_flutter_android: &&
 cd ../webview_flutter_android &&
 flutter pub get &&
-flutter pub run mplat_pr_tools publish \
-    --no-createGithubRelease &&
 # Do not create github release until the very last step
+mplat_dart publish --no-github-release &&
 
 echo &&
 echo webview_flutter_wkwebview: &&
 cd ../webview_flutter_wkwebview &&
 flutter pub get &&
-flutter pub run mplat_pr_tools publish \
-    --no-createGithubRelease &&
 # Do not create github release until the very last step
+mplat_dart publish --no-github-release &&
 
 echo &&
 echo webview_flutter_web: &&
 cd ../webview_flutter_web &&
 flutter pub get &&
-flutter pub run mplat_pr_tools publish \
-    --no-createGithubRelease &&
 # Do not create github release until the very last step
+mplat_dart publish --no-github-release &&
 echo 'sleeping for '$SLEEP_TIME' seconds to wait for packages to synchronize because webview_flutter depends on all of the above packages...' && 
 sleep $SLEEP_TIME &&
 
@@ -46,6 +42,5 @@ echo &&
 echo webview_flutter_platform_interface: &&
 cd ../webview_flutter &&
 flutter pub get &&
-flutter pub run mplat_pr_tools publish \
-    --no-createGithubRelease
-# NOTICE the createGithubRelease is not currently working in mplat_pr_tools for this repo. Not sure why -mskolnick
+mplat_dart publish --no-github-release
+# Need to test out the mplat_dart github-release for this repo. mplat_pr_tools github release was not working here -mskolnick
