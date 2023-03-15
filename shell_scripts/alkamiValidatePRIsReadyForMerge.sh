e13b04f7046c7dd776135cb4742266ea9115a44f
Micaiah Skolnick <mskolnickdev@gmail.com>
2023-03-15 13:39:00 -0400

    chore:DEV-151337 rename mplat_dart to mplat (#14)

--- shell_scripts/alkamiValidatePRIsReadyForMerge.sh
+++ shell_scripts/alkamiValidatePRIsReadyForMerge.sh
@@ -5,28 +5,28 @@ echo &&
 echo webview_flutter_platform_interface: &&
 cd webview_flutter_platform_interface &&
 flutter pub get &&
-mplat_dart validate-pr &&
+mplat validate-pr &&
 
 echo &&
 echo webview_flutter_android: &&
 cd ../webview_flutter_android &&
 flutter pub get &&
-mplat_dart validate-pr &&
+mplat validate-pr &&
 
 echo &&
 echo webview_flutter_wkwebview: &&
 cd ../webview_flutter_wkwebview &&
 flutter pub get &&
-mplat_dart validate-pr &&
+mplat validate-pr &&
 
 echo &&
 echo webview_flutter_web: &&
 cd ../webview_flutter_web &&
 flutter pub get &&
-mplat_dart validate-pr &&
+mplat validate-pr &&
 
 echo &&
 echo webview_flutter: &&
 cd ../webview_flutter &&
 flutter pub get &&
-mplat_dart validate-pr
+mplat validate-pr
